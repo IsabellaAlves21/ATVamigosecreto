24568027bea199962b1cc5f9b843f09af0e5be72
ISABELLA ALVES DA SILVA LIMA TAVARES <isabella.asltavares@senacsp.edu.br>
2024-06-21 14:13:31 -0300

    botoes funcionando + atv finalizada

--- JS/main.js
+++ JS/main.js
@@ -1,64 +1,69 @@
 let amigos = [];
-
+ 
 const btnAdicionar = document.querySelector('#btn-adicionar');
-const participantes = document.querySelector('#friends-selection');
-const txtAmigo = document.querySelector('.friend-name');
-const mensagemErro = document.querySelector('.error');
-const btnSorteio = document.querySelector('#btn-sortear');
-const btnReiniciar = document.querySelector('#btn-reiniciar');
-const result = document.querySelector('.results');
-
-
-console.log(btnAdicionar, participantes, txtAmigo, mensagemErro, btnSorteio, btnReiniciar); 
-
-btnAdicionar.addEventListener('click', () => {
-    console.log('Adicionar botão clicado'); 
-    if (txtAmigo.value !== '') {
-        mensagemErro.innerHTML = "";
-        if (participantes.textContent.length === 0) {
-            participantes.innerHTML = txtAmigo.value;
+const participantes = document.querySelector('#friends-selection')
+const txtAmigo = document.querySelector('.friend-name')
+const mensagemErro = document.querySelector(".error")
+const btnSorteio = document.querySelector('#btn-sortear')
+const btnReiniciar = document.querySelector('#btn-reiniciar')
+ 
+btnAdicionar.addEventListener('click',()=>{
+    if (txtAmigo.value!=='') {
+        mensagemErro.innerHTML = ""
+        if (participantes.textContent.length===0) {
+        participantes.innerHTML=txtAmigo.value;
         } else {
-            participantes.textContent = participantes.textContent + "," + txtAmigo.value;
-        }
-        amigos.push(txtAmigo.value);
-        console.log(amigos);
-        txtAmigo.value = "";
-    } else {
-        mensagemErro.innerHTML = 'ERRO! Insira um valor válido!';
+        participantes.textContent=participantes.textContent + "," +txtAmigo.value;
     }
-});
-
-btnReiniciar.addEventListener('click', () => {
-    console.log('Reiniciar botão clicado'); 
-    amigos = [];
-    participantes.innerHTML = "";
-    txtAmigo.value = "";
+    amigos.push(txtAmigo.value);
+    console.log(amigos);
+    txtAmigo.value="";
     mensagemErro.innerHTML = "";
-    result.innerHTML = '';
-    console.log("Lista de amigos reiniciada:", amigos);
-});
-
-function embaralhar(lista) {
-    for (let indice = lista.length; indice; indice--) {
-        const indiceAleatorio = Math.floor(Math.random() * indice);
-        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
-    }
-}
-
-function sortear() {
-    if (amigos.length < 4) {
-        mostrarMsgErro('Adicione ao menos 4 amigos!');
-        return;
+    } else {
+        mensagemErro.innerHTML ='ERRO! Insira um valor válido!';
     }
  
+})
+ 
+btnSorteio.addEventListener('click',()=>{
+    if (amigos.length>=4) {
+        sortear();
+        mensagemErro.innerHTML ='';
+       } else {
+        mensagemErro.innerHTML ='ERRO! Insira um valor válido!';
+       }
+    })    
+ 
+function sortear() {
     embaralhar(amigos);
  
-    let sorteio = document.querySelector('.results');
+    let result = document.querySelector('.results');
     for (let i = 0; i < amigos.length; i++) {
         if (i == amigos.length - 1) {
-            sorteio.innerHTML = sorteio.innerHTML + '<li>' + amigos[i] + ' --> ' + amigos[0] + '</li>';
+            result.innerHTML = result.innerHTML + '<li>' + amigos[i] + ' --> ' + amigos[0] + '</li>';
         } else {
-            sorteio.innerHTML = sorteio.innerHTML + '<li>' + amigos[i] + ' --> ' + amigos[i + 1] + '</li>';
+            result.innerHTML = result.innerHTML + '<li>' + amigos[i] + ' --> ' + amigos[i + 1] + '</li>';
         }
     }
 }
+ 
+function embaralhar(amigos) {
+    for (let indice = amigos.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [amigos[indice - 1], amigos[indiceAleatorio]] = [amigos[indiceAleatorio], amigos[indice - 1]];
+    }
+}
+ 
+btnReiniciar.addEventListener('click', () => {
+    participantes.textContent = '';
+ 
+    txtAmigo.value = '';
+ 
+    mensagemErro.innerHTML = '';
+ 
+    amigos = [];
+ 
+    result.innerHTML = '';
+ 
+    mensagemErro.innerHTML ='Resetado!';
+});
